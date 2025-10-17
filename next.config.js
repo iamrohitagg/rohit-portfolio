a8f20d3c5a4f53556e33a8c5232a44eec7e75841
Rohit <rohit@classics-MacBook-Air.local>
2025-10-17 20:21:20 +0530

    fix: google owner file added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+        permanent: false,
+      },
+    ];
+  },
+};
